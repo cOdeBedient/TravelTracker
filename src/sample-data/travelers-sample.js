ec712abbadde3dc2d5072b5c75e17bce9ebd088a
Adam Bedient <adambedient@gmail.com>
2024-02-28 15:14:59 -0500

    declare test data for each category

---
+++ src/sample-data/travelers-sample.js
@@ -0,0 +1,32 @@
+[
+    {
+      id: 1,
+      name: "John Smith",
+      travelerType: "relaxer"
+    },
+    {
+      id: 2,
+      name: "Emily Johnson",
+      travelerType: "thrill-seeker"
+    },
+    {
+      id: 3,
+      name: "Michael Williams",
+      travelerType: "shopper"
+    },
+    {
+      id: 4,
+      name: "Jessica Brown",
+      travelerType: "history buff"
+    },
+    {
+      id: 5,
+      name: "David Davis",
+      travelerType: "relaxer"
+    },
+    {
+      id: 6,
+      name: "Sarah Miller",
+      travelerType: "thrill-seeker"
+    }
+  ]
---
+++ src/sample-data/trips-sample.js
@@ -0,0 +1,62 @@
+[
+    {
+      id: 1,
+      userID: 3,
+      destinationID: 2,
+      travelers: 6,
+      date: "2024/09/23",
+      duration: 5,
+      status: "approved",
+      suggestedActivities: []
+    },
+    {
+      id: 2,
+      userID: 2,
+      destinationID: 6,
+      travelers: 4,
+      date: "2024/07/13",
+      duration: 2,
+      status: "approved",
+      suggestedActivities: []
+    },
+    {
+      "id: 3,
+      "userID: 4,
+      "destinationID: 5,
+      "travelers: 7,
+      "date: "2024/03/28",
+      "duration: 8,
+      "status: "pending",
+      "suggestedActivities: []
+    },
+    {
+      id: 4,
+      userID: 2,
+      destinationID: 1,
+      travelers: 3,
+      date: "2024/06/10",
+      duration: 6,
+      status: "approved",
+      suggestedActivities: []
+    },
+    {
+      id: 5,
+      userID: 1,
+      destinationID: 6,
+      travelers: 5,
+      date: "2024/10/29",
+      duration: 10,
+      status: "pending",
+      suggestedActivities: []
+    },
+    {
+      id: 6,
+      userID: 6,
+      destinationID: 4,
+      travelers: 1,
+      date: "2024/04/17",
+      duration: 3,
+      status: "pending",
+      suggestedActivities: []
+    }
+  ]
