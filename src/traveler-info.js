3b3650d86132ede73ce59732d9d2977dab18d755
Adam Bedient <adambedient@gmail.com>
2024-02-29 14:05:09 -0500

    Sketch out total cost and date functions

--- src/traveler-info.js
+++ src/traveler-info.js
@@ -19,8 +19,34 @@ function getDestinations(id, trips, destinations) {
     return foundDestinations;
 }
 
+function getDate() {
+    const today = new Date();
+    const todayYear = today.getFullYear();
+    const todayMonth = today.getMonth().toString().padStart(2, '0')
+    const todayDay = today.getDate().toString().padStart(2, '0')
+    return `${todayYear}${todayMonth}${todayDay}`
+}
+
+function computeTotalCost(id, trips, destinations) {
+    const today = getDate();
+    let approvedPastTrips = trips.filter((trip) => {
+        return (trip.status === approved) && trip.date.replace('/', '') === today;
+    });
+    let approvedPastDestinations = getDestinations(id, approvedPastTrips, destinations);
+    let totalCost = approvedPastDestinations.reduce((total, destination) => {
+        total += destination.estimatedLodgingCostPerDay
+        total += destination.estimatedFlightCostPerPerson
+
+        return total;
+    }, 0)
+
+    return totalCost;
+}
+
 export {
     getTraveler,
     getTrips,
-    getDestinations
+    getDestinations,
+    getDate,
+    computeTotalCost
 }
